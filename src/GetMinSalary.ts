a61634c58170593ff7417065bb528a6868a86f04
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-28 15:44:23 -0300

    fix: salary calculus fixed

--- src/GetMinSalary.ts
+++ src/GetMinSalary.ts
@@ -1,8 +1,8 @@
 export class GetMinSalary {
   static run(employeePosition: string): number {
     const minSalary = {
-      operario: Math.floor(Math.random() * (750 - 2500 + 1)) + 750,
-      gerente: Math.floor(Math.random() * (1400 - 4600 + 1)) + 1400,
+      operario: Math.floor(Math.random() * (2500 - 750 + 1)) + 750,
+      gerente: Math.floor(Math.random() * (4600 - 1400 + 1)) + 1400,
     }[employeePosition];
 
     if (!minSalary) throw new Error("Employee position is invalid!");
