8923bba0ff72dcf2475f64375a512e2407edfb2a
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-28 14:20:13 -0300

    refactor: static method implemented

--- src/GetWorkCoefficient.ts
+++ src/GetWorkCoefficient.ts
@@ -1,12 +1,10 @@
 export class GetWorkCoefficient {
-  constructor(private workShift: string) {}
-
-  run(): number {
+  static run(workShift: string): number {
     const workCoefficient = {
       matutino: 0.13,
       vespertino: 0.04,
       noturno: 0.08,
-    }[this.workShift];
+    }[workShift];
 
     if (!workCoefficient) throw new Error("Enter a valid work shift");
 
