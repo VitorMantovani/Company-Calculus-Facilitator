9d6bc2666f925001dd4d54b7a529f0e9ecdd721a
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-28 11:03:26 -0300

    refactor: method return typed

--- src/GetWorkCoefficient.ts
+++ src/GetWorkCoefficient.ts
@@ -1,16 +1,15 @@
 export class GetWorkCoefficient {
-    
-    constructor(private workShift: string) {}
+  constructor(private workShift: string) {}
 
-    run() {
-        const workCoefficient = {
-            matutino: 0.13,
-            vespertino: 0.04,
-            noturno: 0.08
-        }[this.workShift];
+  run(): number {
+    const workCoefficient = {
+      matutino: 0.13,
+      vespertino: 0.04,
+      noturno: 0.08,
+    }[this.workShift];
 
-        if (!workCoefficient) throw new Error("Enter a valid work shift");
+    if (!workCoefficient) throw new Error("Enter a valid work shift");
 
-        return workCoefficient;
-    }
+    return workCoefficient;
+  }
 }
