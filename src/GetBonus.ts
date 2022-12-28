12edc0e9dee7e3260ee88c8de8e63d5ac958b179
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-28 16:31:22 -0300

    refactor: changed workShift  parameter type

--- src/GetBonus.ts
+++ src/GetBonus.ts
@@ -1,5 +1,5 @@
 export class GetBonus {
-  static run(workShift: string, workedHours: number) {
-    return workShift === "matutino" && workedHours > 80 ? 1200 : 465
+  static run(workShift: string[], workedHours: number) {
+    return workShift.includes("matutino") && workedHours > 80 ? 1200 : 465
   }
 }
